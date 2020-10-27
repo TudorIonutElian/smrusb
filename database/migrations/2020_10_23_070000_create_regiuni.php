ff33166528a49ccf22f31d04646a09aff93f0c3a
TudorIonutElian <ionuteliantudor@gmail.com>
2020-10-27 17:15:57 +0200

    Testing Inserts

--- database/migrations/2020_10_23_070000_create_regiuni.php
+++ database/migrations/2020_10_23_070000_create_regiuni.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateRegiuni extends Migration
 {
@@ -17,6 +18,19 @@ class CreateRegiuni extends Migration
             $table->id();
             $table->string('denumire', 40);
         });
+
+        DB::table('regiuni')->insert(
+            array(
+                'denumire'  => 'Regiunea Nord-Est',
+                'denumire'  => 'Regiunea Sud-Est',
+                'denumire'  => 'Regiunea Sud - Muntenia',
+                'denumire'  => 'Regiunea Sud-Vest Oltenia',
+                'denumire'  => 'Regiunea Vest',
+                'denumire'  => 'Regiunea Nord-Vest',
+                'denumire'  => 'Regiunea Centru',
+                'denumire'  => 'Regiunea București - Ilfov'
+            )
+        );
     }
 
     /**
